ce97f0742187c4aadc491c4a8dbdd1c084b9ec80
Michael Ke <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-08-22 18:33:57 -0700

    feat: refactor, permit ID override, updated output method, additional metadata output
    Refactored main function to reduce the amount of code repetition.
    
    Added a check for filename ID and file ID to see if they differ; if they do, change the file ID to filename ID. This allows for manual renaming of ab1 files and for those changes to be reflected in the fasta files.
    
    SeqIO.write method is now solely used for output of the fasta files.
    
    Metadata output now includes the trace score and median PUP scores to permit ease of analysis. Additionally, the metadata rows are sorted by primer, then by sample name.

--- sanger-processing/sanger-sequence-trim.py
+++ sanger-processing/sanger-sequence-trim.py
@@ -1,64 +1,100 @@
-import argparse
-import pathlib
+"""
+Author : Michael Ke
+Date   : 2022-08-22
+Purpose: From ab1 files, trims and outputs sequence as fasta files. 
+"""
+from argparse import ArgumentParser
+from pathlib import Path
 import csv
 from Bio import SeqIO
 
 def parse_args(): 
-    parser = argparse.ArgumentParser(description='Process ab1 files and get Mott algorithm-trimmed sequences')
+    parser = ArgumentParser(
+        description='Process ab1 files and get Mott algorithm-trimmed sequences',
+        epilog='V1.0.0'
+        )
     parser.add_argument(
         'ab1_path', 
-        metavar='path_to_ab1_files', 
         action='store', 
-        type=pathlib.Path,
+        type=Path,
         help = 'path to ab1 sequence files'
     )
     parser.add_argument(
-        '-s', '--sep',
-        dest='sep_flag',
+        '-c', '--concat',
+        dest='con_flag',
         action='store_true',
-        help='Flag to create separate files for each sequence'
+        help='Flag to concatenate by primers'
+    )
+    parser.add_argument(
+        '-o', '--output',
+        dest='output_path',
+        action='store',
+        default=None,
+        type=Path, 
+        help='output path for metadata file and fastas'
     )
     args = parser.parse_args()
-    return (args.ab1_path, args.sep_flag)
+
+    if not(args.output_path): 
+        args.output_path = args.ab1_path
+
+    return (args)
 
 def main():
-    ab1_path, sep_flag = parse_args()
+    
+    args = parse_args()
+    ab1_path = args.ab1_path
+    con_flag = args.con_flag
+    output_path = args.output_path
     metadata = [] 
-    if sep_flag is True:
+
+    ab1_data = []
+    ab1_file_paths = []
+
+    #Get all ab1 files in directory
+    try: 
         ab1_iter = ab1_path.glob('*.ab1')
-        for ab1_file_path in ab1_iter:
-            #Open
-            ab1_file = SeqIO.read(ab1_file_path, 'abi')
-            ab1_file_trim = SeqIO.read(ab1_file_path, 'abi-trim')
-            #Check trims
-            left_trim = ab1_file.seq.find(ab1_file_trim.seq[0:5])-1
-            right_trim = len(ab1_file.seq) - len(ab1_file_trim) - left_trim
-            metadata.append((ab1_file_trim.id, left_trim, right_trim))
-            #Output
-            output_path = ab1_file_path.with_name(f'{ab1_file_trim.id}_trimmed.fasta')
-            SeqIO.write(ab1_file_trim, output_path, 'fasta')
-    else: 
-        output_path = ab1_path.joinpath('output.fasta')
-        output_file = open(output_path, 'w')
-        ab1_iter = ab1_path.glob('*.ab1')
-        for ab1_file_path in ab1_iter:
-            #Open
-            ab1_file = SeqIO.read(ab1_file_path, 'abi') 
-            ab1_file_trim = SeqIO.read(ab1_file_path, 'abi-trim')
-            #Check trims
-            left_trim = ab1_file.seq.find(ab1_file_trim.seq[0:5])-1
-            right_trim = len(ab1_file.seq) - len(ab1_file_trim) - left_trim
-            metadata.append((ab1_file_trim.id, left_trim, right_trim))
-            #Output
-            output_file.write(f'>{ab1_file_trim.id}\n')
-            output_file.write(f'{str(ab1_file_trim.seq)}\n')
-        output_file.close()
-    metadata_path = ab1_path.joinpath('metadata.csv')
-    metadata_file = open(metadata_path, 'w', newline='')
-    csvwriter = csv.writer(metadata_file)
-    csvwriter.writerow(('ID','left_trim','right_trim'))
-    csvwriter.writerows(metadata)
-    metadata_file.close()
+        for ab1_file_path in ab1_iter: 
+            ab1_file_paths.append(ab1_file_path)
+    except: 
+        #TODO: HANDLE EXCEPTIONS
+        pass
+
+    for ab1_file_path in ab1_file_paths:
+        ab1_file = SeqIO.read(ab1_file_path, 'abi')
+        ab1_file_trim = SeqIO.read(ab1_file_path, 'abi-trim')
+
+        #Determine how much Mott's trimming algorithm removed from the sequence.
+        #Left trim is found by searching the first 5 nucleotides of the trimmed sequence
+        #in the original file. 
+        left_trim = ab1_file.seq.find(ab1_file_trim.seq[0:5])-1
+        right_trim = len(ab1_file.seq) - len(ab1_file_trim) - left_trim
+
+        #To allow for manual correction of ID, the ab1 file name ID is used instead of the ab1 file ID
+        ab1_file_id = '_'.join(ab1_file_path.stem.split('_')[0:2])
+        if ab1_file_trim.id != ab1_file_id: 
+            print(f'{ab1_file_path.stem} ID differs from filename ID. Changing ID to filename ID..')
+            ab1_file_trim.id = ab1_file_id
+        #quality_score = sum(ab1_file.letter_annotations['phred_quality'])/len(ab1_file.letter_annotations['phred_quality'])
+        metadata.append((ab1_file_trim.id, ab1_file.annotations['abif_raw']['TrSc1'], ab1_file.annotations['abif_raw']['PuSc1'], left_trim, right_trim))
+        ab1_data.append(ab1_file_trim)
+
+    #Output fasta files
+    if con_flag: 
+        fasta_output_path = output_path.joinpath(f'all_sequences.fasta') 
+        SeqIO.write(ab1_data, fasta_output_path, 'fasta')
+    else:  
+        for data in ab1_data: 
+            fasta_output_path = output_path.joinpath(f'{data.id}_trimmed.fasta')
+            SeqIO.write(data, fasta_output_path, 'fasta')
+
+    #Output metadata
+    metadata_path = output_path.joinpath('metadata.csv')
+    metadata.sort(key= lambda x : (x[0].split('_')[1], x[0].split('_')[0]))
+    with open(metadata_path, 'w', newline='') as metadata_file:  
+        csvwriter = csv.writer(metadata_file)
+        csvwriter.writerow(('ID', 'trace_score', 'median_pup', 'left_trim','right_trim'))
+        csvwriter.writerows(metadata)
 
 if __name__ == '__main__':
     main()
