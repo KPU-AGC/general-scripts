1db2bd70edb97b4e70af13eca1dec4ecad4d8b43
cherrytrees-kpu <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-08-23 12:10:10 -0700

    feat: created output_fastas function, added ability to filter sequences by QC values
    Created output_fastas function to simplify main.
    
    Added ability to filter by quality values (default: sequences > 30 trace score, >10 median PUP).

--- sanger-processing/sanger-sequence-trim.py
+++ sanger-processing/sanger-sequence-trim.py
@@ -8,6 +8,26 @@ from pathlib import Path
 import csv
 from Bio import SeqIO
 
+def output_fastas(ab1_data: list, output_path: Path, con_flag: bool) -> None: 
+    if con_flag: 
+        #Determine which primers are present
+        primer_data = dict()
+        for data in ab1_data: 
+            primer_name = data.id.split('_')[1]
+            if primer_name not in primer_data.keys(): 
+                primer_data[primer_name] = []
+            else: 
+                primer_data[primer_name].append(data)
+        
+        #Write individual primer data
+        for primer_name in primer_data.keys(): 
+            fasta_output_path = output_path.joinpath(f'{primer_name}_sequences.fasta')
+            SeqIO.write(primer_data[primer_name], fasta_output_path, 'fasta')
+    else:  
+        for data in ab1_data: 
+            fasta_output_path = output_path.joinpath(f'{data.id}_trimmed.fasta')
+            SeqIO.write(data, fasta_output_path, 'fasta')
+
 def parse_args(): 
     parser = ArgumentParser(
         description='Process ab1 files and get Mott algorithm-trimmed sequences',
@@ -15,15 +35,9 @@ def parse_args():
         )
     parser.add_argument(
         'ab1_path', 
-        action='store', 
+        action='store',
         type=Path,
-        help = 'path to ab1 sequence files'
-    )
-    parser.add_argument(
-        '-c', '--concat',
-        dest='con_flag',
-        action='store_true',
-        help='Flag to concatenate by primers'
+        help='Path to ab1 sequence files.'
     )
     parser.add_argument(
         '-o', '--output',
@@ -31,8 +45,38 @@ def parse_args():
         action='store',
         default=None,
         type=Path, 
-        help='output path for metadata file and fastas'
+        help='Output path for metadata file and fastas.'
     )
+    parser.add_argument(
+        '-c', '--concat',
+        dest='con_flag',
+        action='store_true',
+        help='Flag to concatenate by primers.'
+    )
+    qc_group = parser.add_argument_group('qc_options')
+    qc_group.add_argument(
+        '-q', '--filter_qc',
+        dest='qc_flag', 
+        action='store_true',
+        help='Flag to filter by trace score and median PUP score.'
+    )
+    qc_group.add_argument(
+        '-t', '--min_trace',
+        dest='min_trace',
+        action='store',
+        type=int,
+        default=30,
+        help='Minimum trace score for a sequence to be passed.',
+    )
+    qc_group.add_argument(
+        '-p', '--min_pup', 
+        dest='min_pup', 
+        action='store',
+        type=int,
+        default=10,
+        help='Minimum median PUP score for a sequence to be passed.'
+    )
+    
     args = parser.parse_args()
 
     if not(args.output_path): 
@@ -45,6 +89,9 @@ def main():
     args = parse_args()
     ab1_path = args.ab1_path
     con_flag = args.con_flag
+    qc_flag = args.qc_flag
+    min_trace = args.min_trace
+    min_pup = args.min_pup
     output_path = args.output_path
     metadata = [] 
 
@@ -70,23 +117,24 @@ def main():
         left_trim = ab1_file.seq.find(ab1_file_trim.seq[0:5])-1
         right_trim = len(ab1_file.seq) - len(ab1_file_trim) - left_trim
 
+        #TODO: validation of proper filename convention
         #To allow for manual correction of ID, the ab1 file name ID is used instead of the ab1 file ID
         ab1_file_id = '_'.join(ab1_file_path.stem.split('_')[0:2])
         if ab1_file_trim.id != ab1_file_id: 
-            print(f'{ab1_file_path.stem} ID differs from filename ID. Changing ID to filename ID..')
+            print(f'{ab1_file_trim.id} differs from filename {ab1_file_id}. Changing ID to filename ID..')
             ab1_file_trim.id = ab1_file_id
         #quality_score = sum(ab1_file.letter_annotations['phred_quality'])/len(ab1_file.letter_annotations['phred_quality'])
         metadata.append((ab1_file_trim.id, ab1_file.annotations['abif_raw']['TrSc1'], ab1_file.annotations['abif_raw']['PuSc1'], left_trim, right_trim))
-        ab1_data.append(ab1_file_trim)
+        if not qc_flag:
+            ab1_data.append(ab1_file_trim)
+        else: 
+            if (
+                ab1_file.annotations['abif_raw']['TrSc1'] > min_trace 
+                and ab1_file.annotations['abif_raw']['PuSc1'] > min_pup
+            ): 
+                ab1_data.append(ab1_file_trim)
 
-    #Output fasta files
-    if con_flag: 
-        fasta_output_path = output_path.joinpath(f'all_sequences.fasta') 
-        SeqIO.write(ab1_data, fasta_output_path, 'fasta')
-    else:  
-        for data in ab1_data: 
-            fasta_output_path = output_path.joinpath(f'{data.id}_trimmed.fasta')
-            SeqIO.write(data, fasta_output_path, 'fasta')
+    output_fastas(ab1_data, output_path, con_flag)
 
     #Output metadata
     metadata_path = output_path.joinpath('metadata.csv')
