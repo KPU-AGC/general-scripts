abfc145ac2d48559d54a995c781a7a2312f99e8c
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2022-10-04 15:55:18 -0700

    BREAKING CHANGE!: full refactor
    - new v1.0.0, now follows style guide
    - should maintain identical functionality
    - dry except is ambiguous -- i tried to handle more specific exceptions

--- blast/generate_taxid_map.py
+++ blast/generate_taxid_map.py
@@ -1,54 +1,93 @@
+#!/usr/bin/env python3
 """
-Generate a taxid map file for making a custom BLAST db
+Purpose: Generate a taxid map file for making a custom BLAST db
 """
+__author__ = "Michael Ke; Erick Samera"
+__version__ = "1.0.0"
+__comment__ = 'stable enough'
+
+# MODULES
+# --------------------------------------------------
+from argparse import (
+    Namespace,
+    ArgumentParser,
+    RawDescriptionHelpFormatter)
+from pathlib import Path
+# --------------------------------------------------
 from Bio import SeqIO
-import pathlib
-import argparse
+# --------------------------------------------------
+# ARGPARSE
+# --------------------------------------------------
+def get_args() -> Namespace:
+    """ Get command-line arguments """
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Generate a taxid map from .gb file data')
-    parser.add_argument('f', 
-        metavar='input', 
-        action='store', 
-        type=pathlib.Path, 
-        help = 'name of input .gb file'
-    )
-    parser.add_argument('-o',
-        dest='output_path', 
-        action='store', 
-        type=pathlib.Path,
+    parser = ArgumentParser(
+        #usage='%(prog)s',
+        description="Generate a taxid map from .gb file data.",
+        epilog=f"v{__version__} : {__author__} | {__comment__}",
+        formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'input_path',
+        type=Path,
+        help="path of input (.gb) file")
+    parser.add_argument(
+        '-o',
+        '--out',
+        dest='output_path',
+        metavar='PATH',
+        type=Path,
         default=None,
-        help='output path'
-    )
+        help="path of output directory (Default: in-place)")
+
     args = parser.parse_args()
-    output_path = args.f.parents
-    if args.output_path: 
-        output_path = args.output_path
-    return (args.f, output_path)
 
+    # HANDLE EXCEPTIONS
+    # --------------------------------------------------
+    args.input_path = args.input_path.resolve()
+
+    # throw an error if the input file is not a file
+    if not args.input_path.is_file():
+        parser.error("Your input is not a file.")
+
+    # throw an error if the input file doesn't exist
+    if not args.input_path.exists():
+        parser.error("The input (.gb) file doesn't exist.") 
+
+    # resolve the output path if it's defined, else use the working directory
+    if args.output_path:
+        args.output_path = args.output_path.resolve()
+    else:
+        args.output_path = Path.cwd()
+
+    return args
+
+# --------------------------------------------------
 def main():
-    genbank_filepath, output_path = parse_args()
-    num_seq = 0
-    num_unidentified = 0
-    #Open Genbank file
-    taxid_map_path = output_path.joinpath(genbank_filepath.stem + '_taxid_map.txt')
-    f = open(taxid_map_path,'w')
-    #Map db_xref to accesion
-    for gb in SeqIO.parse(genbank_filepath,"gb"):
-        try:
-            annotations = gb.annotations['accessions'][0]
-            for db_xref in gb.features[0].qualifiers['db_xref']:
-                if db_xref.split(':')[0] == 'taxon': 
-                    taxid = db_xref.split(':')[1]
-            #taxid = gb.features[0].qualifiers['db_xref'][0].split(':')[1]
-            f.write("{} {}\n".format(annotations, taxid))
-            num_seq = num_seq + 1
-        except:
-            num_unidentified = num_unidentified + 1
-            pass
-    f.close()
-    print(f'Total number of sequences: {str(num_seq)}')
-    print(f'Total number of sequences with no species: {str(num_unidentified)}')
+    """ Generate a taxid map from .gb file data. """
+    args = get_args()
+    
+    num_seq: int = 0
+    num_unidentified: int = 0
 
+    taxid_map_path: Path = args.output_path.joinpath(args.input_path.stem + '_taxid_map.txt')
+
+    with open(taxid_map_path, 'w', encoding='UTF8') as taxid_map_file:
+        for genbank_record in SeqIO.parse(args.input_path, 'gb'):
+            try:
+                annotations: str = genbank_record.annotations['accessions'][0]
+                # in db_xref, find the entry with taxon and get the value associated with it
+                taxid: str = [db_xref.split(':')[1] for db_xref in genbank_record.features[0].qualifiers['db_xref'] if 'taxon' in db_xref][0]
+                taxid_map_file.write(f"{annotations} {taxid}\n")
+                num_seq += 1
+            except KeyError:
+                # should handle cases where db_xref or accessions doesn't exist at all
+                num_unidentified += 1
+            except IndexError:
+                # should handle cases where accessions list or taxid list is empty (i.e., no taxon in db_xref)
+                num_unidentified += 1
+
+    print(f'Total number of sequences: {num_seq}')
+    print(f'Total number of sequences with no species: {num_unidentified}')
+# --------------------------------------------------
 if __name__ == "__main__":
     main()
