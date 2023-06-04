6c7676eaf15dad965580ac215b7a3e3e0e2cad2e
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2023-06-03 18:17:35 -0700

    FIX: refactored, more intuitive
    - handles directory input
    - handles dir/*.gb input
    - also handles individual list input
    - also more streamlined implementation

--- miscellaneous/convert-gb-fasta.py
+++ miscellaneous/convert-gb-fasta.py
@@ -1,90 +1,56 @@
 #!/usr/bin/env python3
+__description__ =\
 """
-Author : Michael Ke, Erick Samera
-Date   : 2022-08-22
-Purpose: Extracts sequences from GenBank (.gb) and output as fasta file
+Purpose: Extracts sequences from GenBank (.gb) and output as fasta file.
 """
-
+__author__ = ["Michael Ke", "Erick Samera"]
+__version__ = "2.0.0"
+__comments__ = "stable enough"
 # --------------------------------------------------
 from argparse import (
     Namespace,
     ArgumentParser,
-    RawDescriptionHelpFormatter)
+    RawTextHelpFormatter)
 from pathlib import Path
+# --------------------------------------------------
 from Bio import SeqIO
 # --------------------------------------------------
 def get_args() -> Namespace:
     """ Get command-line arguments """
 
     parser = ArgumentParser(
-        description='Program takes a .gb file/directory of .gb files and outputs .fasta',
-        #usage='%(prog)s',
-        epilog='v1.1.0: Michael Ke, Erick Samera',
-        formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument(
-        'input_path',
+        description=__description__,
+        epilog=f"v{__version__} : {__author__} | {__comments__}",
+        formatter_class=RawTextHelpFormatter)
+    parser.add_argument('input',
+        metavar='<PATH>',
         type=Path,
-        help='path of .gb file/directory of .gb files')
-    parser.add_argument(
-        '-o',
-        '--out',
-        dest='output_path',
+        nargs='+',
+        help='path of GenBank file (.gb) or directory containing Genbank files')
+    parser.add_argument('-o', '--out', dest='output_path',
         type=Path,
         default=None,
-        help='directory to output .fasta files -- output in-place otherwise')
+        help='directory to output .fasta files -- output in current working directory otherwise')
+
+    # --------------------------------------------------
     args = parser.parse_args()
 
-    # resolve the input path
-    args.input_path = args.input_path.resolve()
-
-    # resolve the output path if it's defined
-    if args.output_path:
-        args.output_path = args.output_path.resolve()
+    if len(args.input)==1 and args.input[0].is_dir(): args.input = [file for file in args.input[0].glob('*.gb')]
+    args.input = [file_path for file_path in args.input if Path(file_path).is_file()]
+    if not args.output_path: args.output_path = Path.cwd()
+    args.output_path.mkdir(exist_ok=True)
+    if not args.input: parser.error("Invalid input. No files detected!")
 
     return args
 # --------------------------------------------------
 def main():
-    """ main """
+    """ Does the thing. """
+
     args = get_args()
-    # if the input is a file
-    if args.input_path.is_file():
-        try:
-            # set the output path
-            if args.output_path:
-                output_path = args.output_path
-            else:
-                output_path = args.input_path.parent
 
-            # read and then write
-            genbank_file = SeqIO.read(args.input_path, 'gb')
-            SeqIO.write(
-                genbank_file,
-                output_path.joinpath(f'{args.input_path.stem}.fasta'),
-                'fasta')
-        except ValueError:
-            print(f"ERROR: {args.input_path.name} may not be a GenBank (.gb) file.")
-    # if the input is a directory
-    elif args.input_path.is_dir():
-        for genbank_file_path in args.input_path.glob('*.gb'):
-            try:
-                # set the output path
-                if args.output_path:
-                    output_path = args.output_path
-                else:
-                    output_path = args.input_path
-
-                # read and then write
-                genbank_file = SeqIO.read(genbank_file_path, 'gb')
-                SeqIO.write(
-                    genbank_file,
-                    output_path.joinpath(f'{genbank_file_path.stem}.fasta'),
-                    'fasta')
-            except ValueError:
-                print(f"ERROR: {genbank_file.name} may not be a GenBank (.gb) file.")
-
-    # if the input is somehow neither a file nor a directory
-    else:
-        print("FATAL ERROR: Your input was neither a file nor a directory!")
+    for genbank_file in args.input: 
+        try: SeqIO.write(SeqIO.read(genbank_file, 'gb'), f"{genbank_file.stem}.fasta", 'fasta')
+        except ValueError: print(f"ERROR: {genbank_file.name} may not be a GenBank (.gb) file.")
 
 # --------------------------------------------------
 if __name__ == "__main__":
